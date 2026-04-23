b629bd669597e52a1a36c2cc658a9290d47bcdb1
PabloNovaes <ppablo.0028922@gmail.com>
2026-04-23 19:20:58 -0300

    (feat): add CV

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,6 +8,7 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { DATA } from "@/data/resume";
+import { Icons } from "./icons";
 
 export default function Navbar() {
   return (
@@ -68,6 +69,23 @@ export default function Navbar() {
               </Tooltip>
             );
           })}
+
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <a href="cv.pdf">
+              <DockIcon className="rounded-3xl cursor-pointer size-full bg-background p-0 text-muted-foreground hover:text-foreground hover:bg-muted backdrop-blur-3xl border border-border transition-colors">
+                <Icons.file className="fill-muted-foreground size-full cursor-pointer" />
+              </DockIcon>
+            </a>
+          </TooltipTrigger>
+          <TooltipContent
+            side="top"
+            sideOffset={8}
+            className="rounded-xl bg-primary text-primary-foreground px-4 py-2 text-sm shadow-[0_10px_40px_-10px_rgba(0,0,0,0.3)] dark:shadow-[0_10px_40px_-10px_rgba(0,0,0,0.5)]">
+            <p>Read CV</p>
+            <TooltipArrow className="fill-primary" />
+          </TooltipContent>
+        </Tooltip>
         <Separator
           orientation="vertical"
           className="h-2/3 m-auto w-px bg-border"
