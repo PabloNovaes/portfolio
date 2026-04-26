c698b9e4825fd53023bd2715542b7833e3a9faa2
PabloNovaes <ppablo.0028922@gmail.com>
2026-04-26 01:08:18 -0300

    chore: update dependencies and config files

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,25 +1,26 @@
-import { DATA } from '@/data/resume'
-import { clearString } from '@/lib/clear-strinh'
-import { MetadataRoute } from 'next'
+import { DATA } from "@/data/resume";
+import { cleanString } from "@/lib/clear-string";
+import { MetadataRoute } from "next";
 
-const BASE_URL = "https://pablonovaes-me.vercel.app"
+const BASE_URL = "https://pablonovaes-me.vercel.app";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const projectEntries: MetadataRoute.Sitemap = DATA.projects.map(
+    (project) => ({
+      url: `${BASE_URL}/${cleanString(project.title)}`, // Ajuste o path conforme sua estrutura de rotas
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    }),
+  );
 
-    const projectEntries: MetadataRoute.Sitemap = DATA.projects.map((project) => ({
-        url: `${BASE_URL}/${clearString(project.title)}`, // Ajuste o path conforme sua estrutura de rotas
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-    }))
-
-    return [
-        {
-            url: BASE_URL,
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 1.0,
-        },
-        ...projectEntries,
-    ]
+  return [
+    {
+      url: BASE_URL,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1.0,
+    },
+    ...projectEntries,
+  ];
 }
